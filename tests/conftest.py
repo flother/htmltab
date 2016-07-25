61071028589f76845b622756fa98300480b92810
flother <flother@users.noreply.github.com>
2016-07-25 21:21:39 +0000

    Add tests for the '--select' option

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,35 @@
+import click.testing
 import pytest
+
+
+@pytest.fixture(scope="session")
+def runner():
+    return click.testing.CliRunner()
 
 
 @pytest.fixture(scope="session")
 def basic_csv():
     with open("tests/fixtures/basic.csv") as fh:
+        file_contents = fh.read()
+    return file_contents
+
+
+@pytest.fixture(scope="session")
+def three_csv_table_one():
+    with open("tests/fixtures/three_table1.csv") as fh:
+        file_contents = fh.read()
+    return file_contents
+
+
+@pytest.fixture(scope="session")
+def three_csv_table_two():
+    with open("tests/fixtures/three_table2.csv") as fh:
+        file_contents = fh.read()
+    return file_contents
+
+
+@pytest.fixture(scope="session")
+def three_csv_table_three():
+    with open("tests/fixtures/three_table3.csv") as fh:
         file_contents = fh.read()
     return file_contents
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,3 @@
-import click.testing
 from httmock import HTTMock, all_requests
 
 from htmltab.cli import main
@@ -13,20 +12,66 @@ def basic_response(url, request):
             "content": file_contents}
 
 
-def test_local_file(basic_csv):
+def test_local_file(runner, basic_csv):
     """
     Test that the correct CSV data is output when using a local HTML5
     document as input.
     """
-    runner = click.testing.CliRunner()
     result = runner.invoke(main, ["tests/fixtures/basic.html"])
     assert result.exit_code == 0
     assert result.output == basic_csv
 
 
-def test_url(basic_csv):
-    runner = click.testing.CliRunner()
+def test_url(runner, basic_csv):
     with HTTMock(basic_response):
         result = runner.invoke(main, ["http://example.org/basic.html"])
     assert result.exit_code == 0
     assert result.output == basic_csv
+
+
+def test_zero_is_invalid_select_value(runner, three_csv_table_three):
+    result = runner.invoke(main, ["-s", "0", "tests/fixtures/three.html"])
+    assert result.exit_code != 0
+    assert "Error: value matched no elements" in result.output
+
+
+def test_integer_select_value(runner, three_csv_table_three):
+    result = runner.invoke(main, ["--select", "3",
+                                  "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_three
+    result = runner.invoke(main, ["-s", "3", "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_three
+
+
+def test_css_select_value(runner, three_csv_table_two):
+    result = runner.invoke(main, ["--select", "#data table",
+                                  "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_two
+    result = runner.invoke(main, ["-s", "#data table",
+                                  "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_two
+
+
+def test_xpath_select_value(runner, three_csv_table_three):
+    result = runner.invoke(main, ["--select", "(//table)[3]",
+                                  "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_three
+    result = runner.invoke(main, ["-s", "(//table)[3]",
+                                  "tests/fixtures/three.html"])
+    assert result.output == three_csv_table_three
+
+
+def test_invalid_select_value(runner):
+    result = runner.invoke(main, ["-s", "!", "tests/fixtures/three.html"])
+    assert result.exit_code != 0
+    assert "Error: Invalid value: '!'" in result.output
+
+
+def test_table_rows_required(runner):
+    result = runner.invoke(main, ["-s", "#data", "tests/fixtures/three.html"])
+    assert result.exit_code != 0
+    assert "Error: select value matched div element" in result.output
+
+    result = runner.invoke(main, ["-s", "thead", "tests/fixtures/three.html"])
+    assert result.exit_code != 0
+    assert "Error:" in result.output
