d8a3c6caeffe7ab49e4b19e0d5b172632a2f392a
flother <flother@users.noreply.github.com>
2016-07-25 14:06:57 +0000

    Add test for URL as input

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,16 @@
 import click.testing
+from httmock import HTTMock, all_requests
 
 from htmltab.cli import main
+
+
+@all_requests
+def basic_response(url, request):
+    with open("tests/fixtures/basic.html") as fh:
+        file_contents = fh.read()
+    return {"status_code": 200,
+            "text": file_contents,
+            "content": file_contents}
 
 
 def test_local_file(basic_csv):
@@ -10,5 +20,13 @@ def test_local_file(basic_csv):
     """
     runner = click.testing.CliRunner()
     result = runner.invoke(main, ["tests/fixtures/basic.html"])
+    assert result.exit_code == 0
+    assert result.output == basic_csv
+
+
+def test_url(basic_csv):
+    runner = click.testing.CliRunner()
+    with HTTMock(basic_response):
+        result = runner.invoke(main, ["http://example.org/basic.html"])
     assert result.exit_code == 0
     assert result.output == basic_csv
